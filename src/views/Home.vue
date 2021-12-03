859d63bfc50e0861e5582e4a8cc9d5e94b4d09f2
David Mainand <david.mainand@groupama.com>
2021-12-03 19:10:56 +0100

    clean & add chartjs test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <h1>Climatips !</h1>
+  <TestChart></TestChart>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TestChart from '@/components/TestChart.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    TestChart
+  },
+  props: {
+    msg: String,
   },
 });
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

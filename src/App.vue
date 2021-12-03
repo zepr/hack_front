793bf071e303d5c9e12b6aa66ed07c6f1853ad81
David Mainand <david.mainand@groupama.com>
2021-12-03 18:11:31 +0100

    ajout vuestick

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
 </template>
 
 <style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +32,6 @@
     }
   }
 }
+
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(VuesticPlugin);
+app.use(router).mount('#app');

15abad7565a072d12e557e23434414c18cb3938a
Looise <64477904+Looise@users.noreply.github.com>
2021-12-04 11:48:59 +0100

    Create GrapheMarge.vue
    - code graphe marge

---
+++ src/components/GrapheMarge.vue
@@ -0,0 +1,39 @@
+
+
+<template>
+  Affichage du tableau
+  <div id="chart"></div>
+</template>
+
+<script lang="ts">
+
+
+import {defineComponent, onMounted} from 'vue';
+import * as c3 from "c3";
+
+export default defineComponent({
+  name: "GraphMarge",
+  setup: () => {
+
+
+    onMounted( () => {
+      var chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          columns: [
+              ['data1',200, 250, 300], //Gain
+              ['data2', 100, 150], //Perte
+              
+          ],
+          type: 'bar',
+          groups: [                      //un groupe=un scénario
+              ['data1', 'data2']
+          ]
+        }
+      })
+    }
+  },
+})
+
+
+</script>

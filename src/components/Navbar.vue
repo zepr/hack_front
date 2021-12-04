52c82382a64fd7e799a791c0a2ec04677110476e
David Mainand <david.mainand@groupama.com>
2021-12-04 14:40:51 +0100

    not working hero

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,33 @@
 <template>
-  <va-navbar>
-    <template #left>
-      <va-navbar-item></va-navbar-item>
-    </template>
-    <template #center>
-      <va-navbar-item><h1>Climatips</h1></va-navbar-item>
-    </template>
-    <template #right>
-      <va-navbar-item></va-navbar-item>
-    </template>
-  </va-navbar>
+<!--  <div id="container">-->
+<!--    <img id="image" src="@/assets/hackathon_varenne.jpg" />-->
+<!--    <p id="text">-->
+<!--      CLIMATIPS-->
+<!--    </p>-->
+<!--  </div>-->
+
 </template>
 
 <style lang="scss">
-.va-navbar {
-  margin-bottom: 20px;
+#container {
+  width: 100%;
+  position: relative;
+  margin-bottom: 270px
 }
+#image {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+#text {
+  z-index: 100;
+  position: absolute;
+  left: 200px;
+  top: 350px;
+
+  font-weight: bolder;
+  font-size: xxx-large;
+  color: #cccccc;
+}
+
 </style>

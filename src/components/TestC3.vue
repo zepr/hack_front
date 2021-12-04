8d79b822eec0161ba7d372bc85801fcdf0e4072e
Looise <64477904+Looise@users.noreply.github.com>
2021-12-04 01:07:06 +0100

    Update TestC3.vue
    - tableau qui fonctionne
    - grid pour evenement climatique ponctuel
    - axe x: time serie
    - regions pour la plage des dates des evenements climatiques étendus dans le temps (secheresse par ex)

--- src/components/TestC3.vue
+++ src/components/TestC3.vue
@@ -20,15 +20,38 @@ export default defineComponent({
       var chart = c3.generate({
         bindto: '#chart',
         data: {
+          x:'date',
           columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-          ]
-        }
-      });
-    })
-
-  }
+              ['date', '2014-01-01', '2014-01-10', '2014-01-20', '2014-01-30', '2014-02-01'],
+              ['scénario1', 30, 200, 100, 400, 500, 550],
+              ['scénario2', 30, 200, 50, 100, 150, 250]
+          ],
+        },
+        axis: {
+          x: {
+              type: 'timeseries',
+              tick: {
+                format:(x:any) => `${x.getDate()}/${x.getMonth()+1}/${x.getFullYear()}`
+              }
+          }
+          
+        },
+        grid: { // Représenter les axes des épisodes ponctuels
+          x: {
+              lines: [
+                  {value: '2014-01-10' , text: 'Gel'}
+              ]
+          }
+             
+        }, 
+        regions: [ // Représenter les plages des épisodes étendus
+            {start: '2014-01-05', end: '2014-01-10'}
+        ]        
+      },
+      
+    );
+  })
+}
 });
 
 </script>
